cd01196e496da1fb4baf491ebe03ec44bb2074df
Arcdarelle <162192195+Arcdarelle@users.noreply.github.com>
2025-07-02 13:32:08 +0100

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = var.region
+}
+
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = var.cluster_name
+  cluster_version = "1.29"
+  subnets         = var.subnets
+  vpc_id          = var.vpc_id
+
+  node_groups = {
+    default = {
+      desired_capacity = 2
+      max_capacity     = 3
+      min_capacity     = 1
+      instance_types   = ["t3.medium"]
+    }
+  }
+}
